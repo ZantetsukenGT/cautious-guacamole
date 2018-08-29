0fc5f8a5779ae742bdf5a41e9a754ee10c538094
Zantetsuken <25725331+ZantetsukenGT@users.noreply.github.com>
2018-08-29 03:26:28 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+
 #include "Arbol.h"
+#include "Matriz.h"
+#include "esenciales.h"
+
+
 
 int main()
 {
-    Arbol<int> *a1 = new Arbol<int>(21);
-    std::cout << a1->getData();
+    GenerarSeed();
+    Matriz *a1 = new Matriz();
+    for(int i = 0; i < 200; i++)
+    {
+        int j = randomizer(0,20);
+        int k = randomizer(0,50);
+        a1->Insertar(j,k,randomizer(0,1));
+    }
+
+    a1->Imprimir();
     return 0;
 }
