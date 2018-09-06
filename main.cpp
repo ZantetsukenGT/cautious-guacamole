6def62d014d0f6eaf6e157044ba387baa40d70a6
Zantetsuken <25725331+ZantetsukenGT@users.noreply.github.com>
2018-09-06 01:13:49 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 
 #include "Arbol.h"
-#include "Matriz.h"
+#include "MatrizHibrida.h"
 #include "esenciales.h"
-
 
 
 int main()
 {
     GenerarSeed();
-    Matriz *a1 = new Matriz();
-    for(int i = 0; i < 200; i++)
+    MatrizDispersa *a1 = new MatrizDispersa();
+
+    for(int i = 0; i < 500; i++)
     {
-        int j = randomizer(0,20);
-        int k = randomizer(0,50);
-        a1->Insertar(j,k,randomizer(0,1));
+        std::string value = "sex";
+        int j = randomizer(0,10);
+        int k = randomizer(0,10);
+        a1->Insertar(j,k,value);
     }
 
-    a1->Imprimir();
+    /*a1->Imprimir();
+    printf("\n\n");
+    for(int i = 0; i < 500; i++)
+    {
+        int j = randomizer(0,10);
+        int k = randomizer(0,a1->sizecolumnas);
+        a1->Remover(j,k);
+    }
+
+    a1->Imprimir();*/
+
+    Arbol *a2 = new Arbol();
+    Estudiante * e1 = new Estudiante();
+    e1->Imagen = a1;
+    e1->Carnet = 22;//etc
+
+    a2->Insertar(e1);
+    a2->PreOrder();
+
     return 0;
 }
